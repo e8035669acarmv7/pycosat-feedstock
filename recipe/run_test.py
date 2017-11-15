5212d4e5b77722a85a5fc1f59107a7a702d98630
Alan Du <alanhdu@gmail.com>
2017-11-14 20:31:57 -0500

    Pycosat 0.6.3

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -4,7 +4,7 @@ import test_pycosat
 
 assert test_pycosat.run().wasSuccessful()
 
-assert pycosat.__version__ == '0.6.2'
+assert pycosat.__version__ == '0.6.3'
 
 assert test_pycosat.process_cnf_file('qg3-08.cnf') == 18
 assert test_pycosat.process_cnf_file('uf20-098.cnf') == 5
